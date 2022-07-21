ff4f5f0ecb7e5ef42bac45b1275fae3bb5a7ea7d
William Viana <willdev.viana@gmail.com>
2022-07-21 16:37:24 -0300

    learn sintaxe arrow function

---
+++ fundation/anatomy-3.js
@@ -0,0 +1,21 @@
+// Function expression
+const increment1 = function (n) {
+  return n + 1
+}
+
+// Arroy function is always anonymous
+const increment2 = (n) => {
+  return n + 1
+}
+console.log(increment2(1))
+
+const increment3 = n => {
+  return n + 1
+}
+console.log(increment3(10))
+
+const increment4 = n => n + 1
+console.log(increment4(21))
+
+const sum = (a, b) => a + b
+console.log(sum(1, 2))
