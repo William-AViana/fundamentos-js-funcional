4c02d01e311f67a9ef964286649c3b8b85762bcd
William Viana <willdev.viana@gmail.com>
2022-07-25 21:31:39 -0300

    understand firts-class funciton

---
+++ functional/first-class-function.js
@@ -0,0 +1,18 @@
+/*
+ * A programing language is said to have
+ * First-class funcitons when functions in
+ * that language are treated like any other variable.
+ */
+
+const add = (a, b) => a + b
+
+const subtract = (a, b) => a - b
+
+const multiply = (a, b) => a * b
+
+const divide = (x, y) => x / y
+
+console.log(add(10, 20))
+console.log(subtract(10, 20))
+console.log(multiply(10, 20))
+console.log(divide(10, 20))
